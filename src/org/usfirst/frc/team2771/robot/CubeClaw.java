7e4d87cb9a97bb528092fe79f352a1a740f42608
Cymbre <cymbre@mattscott.org>
2018-01-27 15:08:58 -0500

    Started putting in Code Skeleton

---
+++ src/org/usfirst/frc/team2771/robot/CubeClaw.java
@@ -0,0 +1,19 @@
+package org.usfirst.frc.team2771.robot;
+
+public class CubeClaw {
+	public void open() {
+		
+	}
+	public void close() {
+		
+	}
+	public void intakeCube() {
+		
+	}
+	public void ejectCube() {
+		
+	}
+	public void stopIntake() {
+		
+	}
+}
---
+++ src/org/usfirst/frc/team2771/robot/Lift.java
@@ -0,0 +1,14 @@
+package org.usfirst.frc.team2771.robot;
+
+public class Lift {
+	public void move() {
+		
+	}
+	public void goToSwitchHeight() {
+		//The switch is the little one.
+	}
+	public void goToScaleHeight() {
+		//The scale is the big one.
+		//The scale has three different positions, up, down, and level. It could be useful for autonomous.
+	}
+}
