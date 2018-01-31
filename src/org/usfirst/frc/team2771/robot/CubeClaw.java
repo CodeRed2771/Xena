5a1b12e59290562c4ec90f654e576d6777160c74
Cymbre <cymbre@mattscott.org>
2018-01-30 21:02:36 -0500

    Worked on the CubecClaw and Lift Classes

--- src/org/usfirst/frc/team2771/robot/CubeClaw.java
+++ src/org/usfirst/frc/team2771/robot/CubeClaw.java
@@ -1,19 +1,35 @@
 package org.usfirst.frc.team2771.robot;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
 public class CubeClaw {
-	public void open() {
+	TalonSRX leftRollers;
+	TalonSRX rightRollers;
+	
+	
+	public CubeClaw() {
+		leftRollers = new TalonSRX(Wiring.CUBE_CLAW_LEFT_MOTOR);
+		rightRollers = new TalonSRX(Wiring.CUBE_CLAW_RIGHT_MOTOR);
 		
+	}
+	
+	public void open() {
+	
 	}
 	public void close() {
 		
 	}
 	public void intakeCube() {
-		
+		leftRollers.set(ControlMode.PercentOutput, .5);
+		rightRollers.set(ControlMode.PercentOutput, .5);
 	}
 	public void ejectCube() {
-		
+		leftRollers.set(ControlMode.PercentOutput, -.5);
+		rightRollers.set(ControlMode.PercentOutput, -.5);
 	}
 	public void stopIntake() {
-		
+		leftRollers.set(ControlMode.PercentOutput, 0);
+		rightRollers.set(ControlMode.PercentOutput, 0);
 	}
 }
--- src/org/usfirst/frc/team2771/robot/Lift.java
+++ src/org/usfirst/frc/team2771/robot/Lift.java
@@ -1,14 +1,31 @@
 package org.usfirst.frc.team2771.robot;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
 public class Lift {
+	TalonSRX lift1;
+	TalonSRX lift2;
+	
+	public Lift() {
+		lift1 = new TalonSRX(Wiring.LIFT_1);
+		lift2 = new TalonSRX(Wiring.LIFT_2);
+	}
+	
 	public void move() {
+		lift1.set(ControlMode.PercentOutput, .5); //TODO this needs to be changed to take logitech input
+		lift2.set(ControlMode.PercentOutput, .5);
 		
 	}
 	public void goToSwitchHeight() {
 		//The switch is the little one.
+		lift1.set(ControlMode.PercentOutput, .7, 12); //TODO change the number to the correct height.
+		lift2.set(ControlMode.PercentOutput, .7, 12);
 	}
 	public void goToScaleHeight() {
 		//The scale is the big one.
 		//The scale has three different positions, up, down, and level. It could be useful for autonomous.
+		lift1.set(ControlMode.PercentOutput, .7, 64); //TODO change the number to the correct height. 
+		lift2.set(ControlMode.PercentOutput, .7, 64);
 	}
 }
