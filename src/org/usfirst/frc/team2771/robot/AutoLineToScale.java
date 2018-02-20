f04be998540d17d988aa9248e3e0b3302d4adbe8
KamdenBolhuis <kamden.bolhuis@gmail.com>
2018-02-19 19:27:45 -0500

    Added AutoLineToScale - Drive from AutoBaseLine finishing position to scale.

---
+++ src/org/usfirst/frc/team2771/robot/AutoLineToScale.java
@@ -0,0 +1,34 @@
+//Auto line to scale.
+
+package org.usfirst.frc.team2771.robot;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class AutoLineToScale extends AutoBaseClass {
+	public AutoLineToScale(int robotPosition) {
+		super(robotPosition);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void tick() {
+		
+		if (isRunning()) {
+
+			SmartDashboard.putNumber("Auto Step", getCurrentStep());
+
+			switch (getCurrentStep()) {
+			case 0:
+				setTimerAndAdvanceStep(6000);
+				driveInches(65.45, 0, .1);
+				break;
+			case 1:
+				if (driveCompleted())
+					advanceStep();
+				break;
+			case 2:
+				stop();
+				break;
+			}	
+		}
+	}
+}
