10984841e206006e7f73673c91474fae63f5f98c
KamdenBolhuis <kamden.bolhuis@gmail.com>
2018-03-11 14:03:10 -0400

    Added EjectCube to AutoSwitchPlaceCube

--- src/org/usfirst/frc/team2771/robot/AutoSwitchPlaceCube.java
+++ src/org/usfirst/frc/team2771/robot/AutoSwitchPlaceCube.java
@@ -22,29 +22,23 @@ public class AutoSwitchPlaceCube extends AutoBaseClass {
 					turnDegrees(90, .2);
 				else
 					turnDegrees(-90, .2);
-
 				Lift.goSwitch();
-
 				break;
-
 			case 1:
 				if (driveCompleted())
 					advanceStep();
 				break;
-
 			case 2:
 				setTimerAndAdvanceStep(2000);
 				this.driveInches(12, 0, .2);
-
 			case 3:
 				if (driveCompleted())
 					advanceStep();
 				break;
-				
 			case 4:
 				// claw open
+				CubeClaw.ejectCube();
 				advanceStep();
-				
 			case 5:
 				stop();
 				break;
